de7294827bdd7c564e865d00094eb291b9d0daa6
catalystvibe240 <28991126-catalystvibe240@users.noreply.replit.com>
2025-02-21 19:28:59 +0000

    User checkpoint: Integrate Google OAuth authentication using Passport.js and add necessary dependencies.

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref } from "firebase/database";
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyBKxz2XTIo2RaMKlV7xVuXoPB0Z0WqemY4",
